bb168034847b924698a7d542ccf67c1804112cac
rnovikov <v-novir-gri@ae.com>
2017-11-15 22:05:38 +0200

    data class RedditNewsItem

---
+++ app/src/main/java/pandazilla/com/kreddit/RedditNewsItem.kt
@@ -0,0 +1,10 @@
+package pandazilla.com.kreddit
+
+data class RedditNewsItem (
+    val author: String,
+    val title: String,
+    val numComments: Int,
+    val created: Long,
+    val thumbnail: String,
+    val url: String
+)
