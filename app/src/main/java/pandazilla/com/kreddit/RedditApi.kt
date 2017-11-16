67cb3e599e74cf7b576a76f19df40b38c10bc7d5
rnovikov <v-novir-gri@ae.com>
2017-11-16 21:50:43 +0200

    api calls

---
+++ app/src/main/java/pandazilla/com/kreddit/RedditApi.kt
@@ -0,0 +1,11 @@
+package pandazilla.com.kreddit
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface RedditApi {
+    @GET("/top.json")
+    fun getTop(@Query("after") after: String,
+               @Query("limit") limit: String): Call<RedditNewsResponse>
+}
